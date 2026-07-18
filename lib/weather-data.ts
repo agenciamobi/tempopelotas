e8362bd020ee2993163a82d4df11919032eabd3a
agenciamobi <157939955+agenciamobi@users.noreply.github.com>
2026-07-18 16:27:00 -0300

    Amplia dados de chuva, vento e condições noturnas

--- lib/weather-data.ts
+++ lib/weather-data.ts
@@ -1,6 +1,8 @@
 export type WeatherIconName =
   | "sun"
+  | "moon"
   | "partly-cloudy"
+  | "partly-cloudy-night"
   | "cloud"
   | "rain"
   | "storm"
@@ -10,6 +12,8 @@ export type HourlyForecast = {
   time: string;
   temperature: number;
   precipitation: number;
+  windSpeed: number;
+  windGust: number;
   icon: WeatherIconName;
 };
 
@@ -19,6 +23,8 @@ export type DailyForecast = {
   min: number;
   max: number;
   rainChance: number;
+  precipitation: number;
+  windGust: number;
   icon: WeatherIconName;
 };
 
@@ -31,6 +37,7 @@ export type CurrentWeather = {
   humidity: number;
   pressure: number;
   windSpeed: number;
+  windGust: number;
   windDirection: string;
   visibility: number;
   sunrise: string;
@@ -69,6 +76,7 @@ export const fallbackWeatherData: WeatherData = {
     humidity: 86,
     pressure: 1003,
     windSpeed: 12,
+    windGust: 22,
     windDirection: "NNE",
     visibility: 10,
     sunrise: "07:23",
@@ -77,22 +85,22 @@ export const fallbackWeatherData: WeatherData = {
     icon: "partly-cloudy",
   },
   hourly: [
-    { time: "Agora", temperature: 20, precipitation: 0, icon: "partly-cloudy" },
-    { time: "16h", temperature: 20, precipitation: 10, icon: "partly-cloudy" },
-    { time: "17h", temperature: 19, precipitation: 28, icon: "rain" },
-    { time: "18h", temperature: 18, precipitation: 42, icon: "rain" },
-    { time: "19h", temperature: 17, precipitation: 35, icon: "rain" },
-    { time: "20h", temperature: 16, precipitation: 18, icon: "cloud" },
-    { time: "21h", temperature: 16, precipitation: 8, icon: "cloud" },
+    { time: "Agora", temperature: 20, precipitation: 0, windSpeed: 12, windGust: 22, icon: "partly-cloudy" },
+    { time: "16h", temperature: 20, precipitation: 10, windSpeed: 13, windGust: 24, icon: "partly-cloudy" },
+    { time: "17h", temperature: 19, precipitation: 28, windSpeed: 14, windGust: 27, icon: "rain" },
+    { time: "18h", temperature: 18, precipitation: 42, windSpeed: 15, windGust: 30, icon: "rain" },
+    { time: "19h", temperature: 17, precipitation: 35, windSpeed: 13, windGust: 26, icon: "rain" },
+    { time: "20h", temperature: 16, precipitation: 18, windSpeed: 11, windGust: 22, icon: "cloud" },
+    { time: "21h", temperature: 16, precipitation: 8, windSpeed: 10, windGust: 19, icon: "cloud" },
   ],
   daily: [
-    { weekday: "Hoje", date: "18 jul", min: 14, max: 20, rainChance: 38, icon: "partly-cloudy" },
-    { weekday: "Dom", date: "19 jul", min: 13, max: 19, rainChance: 76, icon: "rain" },
-    { weekday: "Seg", date: "20 jul", min: 12, max: 18, rainChance: 68, icon: "rain" },
-    { weekday: "Ter", date: "21 jul", min: 11, max: 17, rainChance: 44, icon: "cloud" },
-    { weekday: "Qua", date: "22 jul", min: 10, max: 18, rainChance: 18, icon: "partly-cloudy" },
-    { weekday: "Qui", date: "23 jul", min: 12, max: 20, rainChance: 12, icon: "sun" },
-    { weekday: "Sex", date: "24 jul", min: 13, max: 21, rainChance: 24, icon: "partly-cloudy" },
+    { weekday: "Hoje", date: "18 jul", min: 14, max: 20, rainChance: 38, precipitation: 2.4, windGust: 35, icon: "partly-cloudy" },
+    { weekday: "Dom", date: "19 jul", min: 13, max: 19, rainChance: 76, precipitation: 12.6, windGust: 48, icon: "rain" },
+    { weekday: "Seg", date: "20 jul", min: 12, max: 18, rainChance: 68, precipitation: 8.2, windGust: 42, icon: "rain" },
+    { weekday: "Ter", date: "21 jul", min: 11, max: 17, rainChance: 44, precipitation: 1.8, windGust: 34, icon: "cloud" },
+    { weekday: "Qua", date: "22 jul", min: 10, max: 18, rainChance: 18, precipitation: 0.2, windGust: 28, icon: "partly-cloudy" },
+    { weekday: "Qui", date: "23 jul", min: 12, max: 20, rainChance: 12, precipitation: 0, windGust: 24, icon: "sun" },
+    { weekday: "Sex", date: "24 jul", min: 13, max: 21, rainChance: 24, precipitation: 0.6, windGust: 29, icon: "partly-cloudy" },
   ],
   regional: [
     { city: "Pelotas", temperature: 20, condition: "partly-cloudy", x: 61, y: 61 },
