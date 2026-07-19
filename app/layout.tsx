74cb5741571de4069b3584846552676bba1156ca
agenciamobi <157939955+agenciamobi@users.noreply.github.com>
2026-07-19 20:54:17 -0300

    style: carregar refinamento transversal por último

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,7 @@ import "./lagoon-monitoring-network.css";
 import "./lagoon-monitoring-api.css";
 import "./hydrology-page-reorganization.css";
 import "./home-page-reorganization.css";
+import "./project-refinement.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
