c9270e875e1354a5e552e3a9668efe1597043aa3
agenciamobi <157939955+agenciamobi@users.noreply.github.com>
2026-07-20 18:00:52 -0300

    Carrega refinamento global do header

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,7 @@ import "./topic-special-pages-refinement.css";
 import "./topic-special-pages-mobile-fix.css";
 import "./topic-weather-pages-refinement.css";
 import "./home-first-fold-operational-v3.css";
+import "./header-template-refinement.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
